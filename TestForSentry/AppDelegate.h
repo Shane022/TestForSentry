d4423114dfc7a89f4de0b8daccf653186f3136ff
shangzhixin <1013956388@qq.com>
2020-06-02 13:23:40 +0800

    Initial Commit

---
+++ TestForSentry/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestForSentry
+//
+//  Created by Shane on 2020/6/2.
+//  Copyright © 2020 Shane. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
